21302c8e9ff23c3c6353794cc9321b09f4d1f553
Lennard <38101963+LennardTFD@users.noreply.github.com>
2019-02-24 15:22:28 +0100

    Add files via upload

--- Uhr/src/StarteUhr.java
+++ Uhr/src/StarteUhr.java
@@ -4,14 +4,14 @@ public class StarteUhr {
 
     public static void main(String[] args) {
         //Create Bildschirm
-        Bildschirm bildschirm = new Bildschirm("Uhr", 900, 280);
+        Bildschirm bildschirm = new Bildschirm("Uhr", 900, 250);
         Uhr clock = new Uhr();
 
         //Refresh Clock and clear Screen
         while(true) {
             bildschirm.löscheAlles();
             clock.zeichneUhr();
-            Hilfe.warte(100);
+            Hilfe.warte(500);
         }
     }
 }
--- Uhr/src/Uhr.java
+++ Uhr/src/Uhr.java
@@ -6,50 +6,26 @@ public class Uhr {
     Block block = new Block();
 
 
-
     public void zeichneUhr()
     {
         int stunde = Hilfe.stunde();
-        //Seperate each Digit of Number
         int[] parsedStunde = {0, 0};
-        //If Number lowet than 10, add a "0" in front
-        if(stunde < 10)
-        {
-            parsedStunde[0] = 0;
-            parsedStunde[1] = stunde;
-        }
-        //Else use current Time but with splitted Digits
-        else
-        {
-            parsedStunde[0] = Integer.parseInt(Integer.toString(stunde).substring(0, 1));
-            parsedStunde[1] = Integer.parseInt(Integer.toString(stunde).substring(1, 2));
-        }
+        parsedStunde[0] = stunde / 10; //Get 10er
+        parsedStunde[1] = stunde - parsedStunde[0] * 10; //Get 1er
 
         int minute = Hilfe.minute();
         int[] parsedMinute = {0, 0};
-        if(minute < 10)
-        {
-            parsedMinute[0] = 0;
-            parsedMinute[1] = minute;
-        }
-        else
-        {
-            parsedMinute[0] = Integer.parseInt(Integer.toString(minute).substring(0, 1));
-            parsedMinute[1] = Integer.parseInt(Integer.toString(minute).substring(1, 2));
-        }
+        parsedMinute[0] = minute / 10;
+        parsedMinute[1] = minute - parsedMinute[0] * 10;
+
 
         int sekunde = Hilfe.sekunde();
         int[] parsedSekunde = {0, 0};
-        if(sekunde < 10)
-        {
-            parsedSekunde[0] = 0;
-            parsedSekunde[1] = sekunde;
-        }
-        else
-        {
-            parsedSekunde[0] = Integer.parseInt(Integer.toString(sekunde).substring(0, 1));
-            parsedSekunde[1] = Integer.parseInt(Integer.toString(sekunde).substring(1, 2));
-        }
+        parsedSekunde[0] = sekunde / 10;
+        parsedSekunde[1] = sekunde - parsedSekunde[0] * 10;
+
+
+
 
         //Draw every number, add a separator each Hour/Minute
         zeichenZahl(0, parsedStunde[0]); //10th Hour
